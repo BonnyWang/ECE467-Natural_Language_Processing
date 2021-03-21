f64721ce8f41f28a5a3d5527301ffeec4715d6f7
BonnyWang <bonny.y.gardy@gmail.com>
2021-03-21 19:44:22 -0400

    hard finish

--- 1-Text_Categorization_Project/main.py
+++ 1-Text_Categorization_Project/main.py
@@ -34,7 +34,7 @@ def getTokens(lines, document_Category, category_Document, doc_Tokens, allTokens
 
         doc_Tokens[doc_Path] = token_Freqency;
 
-
+        # Collect token appearance
         for token in token_Freqency:
             if token in allTokens:
                 allTokens[token] += 1;
@@ -42,7 +42,7 @@ def getTokens(lines, document_Category, category_Document, doc_Tokens, allTokens
                 allTokens[token] = 1;
 
 
-# Calculate the tf*idf value for each document and for each category
+# Calculate the tf*idf value for each document
 def calculateTFIDF(documents,categories, allTokens, doc_Tokens, nDocument, doc_TFIDF):
     for doc in documents:
         tokens = allTokens.keys();
@@ -50,13 +50,15 @@ def calculateTFIDF(documents,categories, allTokens, doc_Tokens, nDocument, doc_T
 
         for token in tokens:
             if token in doc_Tokens[doc]:
-                tf = doc_Tokens[doc][token]/ len(doc_Tokens[doc]);
+                # plus one is chosen arbitarily to improve the performance and add more weight for appeared token
+                tf = doc_Tokens[doc][token]/len(doc_Tokens[doc])+1;
                 idf = math.log10(nDocument/allTokens[token]);
                 tfidf.append(tf*idf);
             else:
                 # So that they have the same size
                 tfidf.append(0);
         doc_TFIDF[doc] = tfidf;
+
 
 # Calculate the weigth value of each token for each category
 def calculateWeight(categories, allTokens, doc_TFIDF, cat_TFIDF):
@@ -67,6 +69,7 @@ def calculateWeight(categories, allTokens, doc_TFIDF, cat_TFIDF):
                 # Summing all the weights for the same token
                 weights[i] = weights[i]+doc_TFIDF[document][i];
         
+        # Normalize the weight
         cat_TFIDF[category] = [weight/len(categories[category]) for weight in weights];      
 
 
@@ -77,7 +80,6 @@ name_Labeled = input("Please enter the name of the list of labeled training docu
 
 # Get information for training
 train_File = open(name_Labeled,"r");
-
 train_Lines = train_File.readlines();
 
 # Create necessary varaibles to hold the data
@@ -97,15 +99,16 @@ train_Doc_TFIDF = {}
 #{doc : [tf*idf]}
 
 train_Cat_TFIDF = {}
-#{Category: tf*idf}
+#{Category: [tf*idf]}
 
 
 getTokens(train_Lines, train_docs, train_categories, train_Doc_Tokens, trian_Tokens);
 calculateTFIDF(train_docs,train_categories,trian_Tokens,train_Doc_Tokens,len(train_Lines),train_Doc_TFIDF);
 calculateWeight(train_categories,trian_Tokens,train_Doc_TFIDF,train_Cat_TFIDF);
 
-classifier = svm.SVC(kernel='linear');
-classifier.fit(numpy.array(list( train_Cat_TFIDF.values())),numpy.array(list( train_Cat_TFIDF.keys())));
+#classifier = svm.SVC(kernel='linear');
+#classifier.fit(numpy.array(list( train_Cat_TFIDF.values())),numpy.array(list( train_Cat_TFIDF.keys())));
+
 print("Trainning Completed!");
 
 # Start predicting the type of documents
@@ -115,7 +118,7 @@ test_File = open(name_Test, "r");
 
 test_Lines = test_File.readlines();
 
-
+# Create necessay variables
 test_Doc_Tokens = {} 
 # {Document: {token: n}}
 test_Tokens = {} 
@@ -132,20 +135,28 @@ test_Cat = {}
 getTokens(test_Lines,test_Doc,test_Cat,test_Doc_Tokens,test_Tokens);
 calculateTFIDF(test_Doc,test_Cat,trian_Tokens,test_Doc_Tokens,len(test_Doc),test_Doc_Tfidf);
 
+# test_Pred[doc] = classifier.predict(numpy.array(list(test_Doc_Tfidf[doc])).reshape(1,-1));
 
-
-# for doc in test_Doc:
-#     test_Pred[doc] = classifier.predict(numpy.array(list(test_Doc_Tfidf[doc])).reshape(1,-1));
-
+# Calculate the similarity and use the max value to predict the category
 for doc in test_Doc:
-    doc_sim = []
-    temp1 = numpy.array(test_Doc_Tfidf[doc])
+    doc_Similarity = [];
+    d = numpy.array(test_Doc_Tfidf[doc]);
     for cat in train_Cat_TFIDF:
-        temp2 = numpy.array(train_Cat_TFIDF[cat])
-        doc_sim.append(numpy.dot(temp1, temp2)/(numpy.linalg.norm(temp1)*numpy.linalg.norm(temp2)))
-    test_Pred[doc] = list(train_Cat_TFIDF)[doc_sim.index(max(doc_sim))]
+        c = numpy.array(train_Cat_TFIDF[cat]);
+
+        # the cosine value is the same as dot product/ norms
+        doc_Similarity.append(numpy.dot(d, c)/(numpy.linalg.norm(d)*numpy.linalg.norm(c)))
+    
+    # Choose the categroy with the largest similarity value
+    test_Pred[doc] = list(train_Cat_TFIDF)[doc_Similarity.index(max(doc_Similarity))]
+
+
+print('Fininshed predicting the category for documents!');
+
 
 output_Name = input('Please enter the name of the output file:');
+
+# Write result to the output file
 out_File = open(output_Name, "w")
 for doc in test_Pred:
     out_File.write(doc + " " + test_Pred[doc] + '\n')
